abed55977b640599d700fa13fe9e81925625c074
Davi Marinho <113703730+Raspadinha06@users.noreply.github.com>
2022-09-16 11:41:14 -0300

    Adicionei os arquivos do Certificard

---
+++ script.js
@@ -0,0 +1,9 @@
+// RESUMO AULA 09
+// Aprendemos a mexer no Figma e transformar o design em código
+// Entendemos melhor como funciona HTML e CSS
+// Estruturamos o nosso portfólio com HTML, aprendendo todas as tags necessárias pra isso
+// Estilizamos o nosso portfólio com CSS, conhecendo os seletores, propriedades e valores necessários para isso
+
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
